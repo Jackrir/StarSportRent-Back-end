09e62b05720beb2a9ce262c3744e2f056aa55de7
Агатін Євген <65392476+Jackrir@users.noreply.github.com>
2021-03-12 22:18:10 +0200

    Добавьте файлы проекта.

---
+++ BusinessLogicLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLogicLayer
+{
+    public static class Class1
+    {
+        public static string work()
+        {
+            return "I am work!";
+        }
+    }
+}
---
+++ StarSportRent/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogicLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "good";
+        }
+    }
+}
